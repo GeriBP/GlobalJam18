95c753e6606c1081e7086f7facc1b82ae0c81778
GeriBP <gbelenguer@gmail.com>
2018-01-26 21:27:49 +0100

    Pre pull

--- VeganBois/Assets/PlayerMove.cs
+++ VeganBois/Assets/PlayerMove.cs
@@ -7,17 +7,41 @@ public class PlayerMove : MonoBehaviour {
     [Header ("Player Movement")]
     [SerializeField]
     float moveSpeed;
+    [SerializeField]
+    GameObject groundP;
+    [SerializeField]
+    LayerMask mask;
 
+    private bool grounded = false;
+    private Vector2 normal;
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
-	}
+        grounded = checkGround();
+    }
 
     private void FixedUpdate()
     {
         
+    }
+
+    private bool checkGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(groundP.transform.position, Vector2.down, 1f, mask);
+        Debug.DrawRay();
+        bool hitBool = false;
+        if (hit.collider != null)
+        {
+            normal = hit.normal;
+            hitBool = true;
+            return hitBool;
+        }
+        else
+        {
+            normal = new Vector2(0, 1);
+            return hitBool;
+        }
     }
 }
