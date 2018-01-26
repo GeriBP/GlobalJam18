f9dd090bdfc6ca55e973282e5cf4e06b834ea0d3
Martiruhay <marti-nini@hotmail.com>
2018-01-26 21:19:59 +0100

    Idk dude

---
+++ VeganBois/Assets/_Scripts/ShootController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour 
+{
+	public GameObject target;
+
+	public float targetDistance;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		Vector2 direction;
+
+		target.transform.position = transform.position + direction * targetDistance;
+	}
+}
