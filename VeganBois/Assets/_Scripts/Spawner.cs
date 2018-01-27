f5255a8f0c6d5112fe01779a2a77bc9479bcb2b6
Martiruhay <marti-nini@hotmail.com>
2018-01-27 21:11:53 +0100

    Spawner wip

---
+++ VeganBois/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject prefab;
+
+	public float minX, maxX, y, spawnTime;
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", 3, spawnTime);
+	}
+
+	void Spawn()
+	{
+		float x = Random.Range (minX, maxX);
+		Vector3 pos = new Vector3 (x, y, 0);
+
+		GameObject g = Instantiate (prefab, pos, Quaternion.identity);
+	}
+}
