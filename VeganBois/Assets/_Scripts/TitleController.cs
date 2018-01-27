fe580422d3d2bff2afcf6c2fbc5d0415107f7ed4
Martiruhay <marti-nini@hotmail.com>
2018-01-27 05:23:21 +0100

    Title screen material design

---
+++ VeganBois/Assets/_Scripts/TitleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour 
+{
+	public void Play1v1()
+	{
+		SceneManager.LoadScene ("Janster");
+	}
+
+	public void Play2v2()
+	{
+		SceneManager.LoadScene ("Janster");
+	}
+
+	public void Exit()
+	{
+		Debug.Log ("Bye!");
+		Application.Quit ();
+	}
+}
+
